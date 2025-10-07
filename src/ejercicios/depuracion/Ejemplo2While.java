33e76b4015028486d9ed533e9ed9210a68264830
sergioag <sergioag@educastur.es>
2025-10-07 15:24:44 +0200

    .gitkeep añadido

--- src/ejercicios/depuracion/Ejemplo2While.java
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -14,7 +14,7 @@ public class Ejemplo2While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, contador = 0;
-        System.out.print("Introduce un número: ");
+        System.out.print("Introduce un número:");
         n = sc.nextInt();
         while (contador< n){
                System.out.println(" * ");
